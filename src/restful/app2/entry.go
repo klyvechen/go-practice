32c38431943e0c2bde68245d7fcd4258ffa01330
klyvechen <klyvechen@gmail.com>
2019-07-06 01:21:30 +0800

    [erp] entry.go

--- src/restful/app2/entry.go
+++ src/restful/app2/entry.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"restful/app2/model"
+	"restful/app2/service"
 
 	"github.com/gorilla/mux"
 )
@@ -15,6 +16,8 @@ func main() {
 	router.HandleFunc("/todos", TodoIndex)
 	router.HandleFunc("/todos/{todoId}", TodoShow)
 	router.HandleFunc("/sidebar", model.GetSideBars)
+	router.HandleFunc("/storeData", service.GetStoreData)
+	router.HandleFunc("/inventoryData", service.GetInventoryData)
 	log.Fatal(http.ListenAndServe(":8003", router))
 }
 
@@ -27,7 +30,12 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	vars := mux.Vars(r)
 	todoId := vars["todoId"]
 	fmt.Fprintln(w, "Todo show:", todoId)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
